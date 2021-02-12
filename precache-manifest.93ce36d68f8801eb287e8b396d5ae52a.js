11e0ad13fb543446e9426868aa52b756cc400921
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2021-02-12 20:24:01 +0800

    Updates

---
+++ precache-manifest.93ce36d68f8801eb287e8b396d5ae52a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539c146af176e771106d4f92ab6115cc",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "1dc79a669357854ac700",
+    "url": "/invoice/static/css/2.9f0ba774.chunk.css"
+  },
+  {
+    "revision": "afbc1e1a2f8e59eec15d",
+    "url": "/invoice/static/css/main.ec7dd9d8.chunk.css"
+  },
+  {
+    "revision": "1dc79a669357854ac700",
+    "url": "/invoice/static/js/2.f96cf465.chunk.js"
+  },
+  {
+    "revision": "6d434ff574ba6e0632e145d5ccc24beb",
+    "url": "/invoice/static/js/2.f96cf465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbc1e1a2f8e59eec15d",
+    "url": "/invoice/static/js/main.064a3b42.chunk.js"
+  },
+  {
+    "revision": "5fb982e8c8615729aefc",
+    "url": "/invoice/static/js/runtime-main.69bc0f1b.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/invoice/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "37df0b6cda81ccba07c05964cf79e9a7",
+    "url": "/invoice/static/media/logo.37df0b6c.png"
+  }
+]);
